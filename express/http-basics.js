0af2850f55b0de5f04a11b6539ce18a1d660aa8e
tumansutradhar <officialbishalsutradhar@gmail.com>
2026-01-01 23:55:40 +0530

    learn: HTTP basics and headers

---
+++ express/http-basics.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Hello World</h1>");
+    res.end();
+});
+
+server.listen(3000);
